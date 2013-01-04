1f44cb796a5243658c171ebf306a7ea9f0537bc0
Matthew Pennell <github@matthewpennell.com>
2013-01-04 21:13:55 +0000

    Add a link to create a new context.

--- application/views/gtd/contexts.blade.php
+++ application/views/gtd/contexts.blade.php
@@ -2,4 +2,5 @@
 	@foreach ($contexts as $context)
 		<li><a href="/gtd/context/{{ $context->id }}">{{ $context->name }}</a></li>
 	@endforeach
+		<li><a href="/gtd/context/new" style="background: none; text-decoration: underline;">Add context</a></li>
 </ul>
