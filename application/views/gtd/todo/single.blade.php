893b09b52161f12bcb8327f113943246c08877cf
Matthew Pennell <github@matthewpennell.com>
2013-01-12 18:41:47 +0000

    Add edit/delete links in task lists, tweak styling.

--- application/views/gtd/todo/single.blade.php
+++ application/views/gtd/todo/single.blade.php
@@ -9,11 +9,9 @@
 		@endif
 		{{ $todo->description }}
 	</a>
-<!--
 	<small style="font-size: 75%;">
 		<a href="/gtd/todo/{{ $todo->id }}/edit">Edit</a>
 		|
-		<a href="/gtd/todo/{{ $todo->id }}/delete">Delete</a>
+		<a href="/gtd/todo/{{ $todo->id }}/delete">Done!</a>
 	</small>
--->
 </li>
